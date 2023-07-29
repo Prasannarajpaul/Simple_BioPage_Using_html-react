bc5c0823f30e486413dd9fa32f423778ec91f1fc
Prasannarajpaul <104220676+Prasannarajpaul@users.noreply.github.com>
2023-07-29 09:40:11 +0530

    Add files via upload

---
+++ elonBio.js
@@ -0,0 +1,27 @@
+ReactDOM.render(
+    <div>
+        <h2>Elon Reeve Musk, born June 28, 1971</h2><hr/>
+        <img src="https://www.thefamouspeople.com/profiles/images/elon-musk-4.jpg" height="200px"/>
+        <ul type="square">
+            <li>He is the founder, chairman, CEO, and chief technology officer of SpaceX</li>
+            <li>Angel investor, CEO and product architect of Tesla, Inc.</li>
+            <li>Owner and CTO of Twitter</li>
+            <li>Founder of the Boring Company</li>
+            <li>Co-founder of Neuralink and OpenAI</li>
+            <li>President of the Musk Foundation</li>
+        </ul>
+        <h2>Business career</h2><hr/>
+        <ul type="square">
+            <li>Zip2</li>
+            <li>X.com and PayPal</li>
+            <li>Space X</li>
+            <li>Starlink</li>
+            <li>Tesla</li>
+            <li>Neuralink</li>
+            <li>The Boring Company</li>
+            <li>Twitter</li>
+            <li>xAI</li>
+        </ul>
+
+    </div>,
+    document.getElementById("content"));
